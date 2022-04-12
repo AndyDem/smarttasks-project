cf8a813147b10c093ef5000c60038e6a09171741
AndyDem <>
2022-04-12 16:18:29 +0300

    adds auth functionality

--- users/views.py
+++ users/views.py
@@ -1,7 +1,22 @@
 from datetime import datetime
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
 
 
 class HomeView(TemplateView):
-    template_name = "users/home.html"
+    template_name = 'users/home.html'
     extra_context = {'today': datetime.today()}
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/signup.html'
+
+
+class LoginInterfaceView(LoginView):
+    template_name = 'users/login.html'
+
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'users/logout.html'
